79f4ea4c5154cf2dafad3c113a9f3c5512facb1c
lucasrangelcosta <lucasrangelcosta@gmail.com>
2024-09-30 15:47:49 -0300

    Cardapio

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontFamily: {
+      'sans': ['Roboto', 'sans-serif']
+    },
+    extend: {
+      backgroundImage: {
+        "home": "url('/assets/bg.png')"
+      }
+    },
+  },
+  plugins: [],
+}
+
